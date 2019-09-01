b386242011c1a9d8919d942ff8d450e86183b1ce
yliu84 <yliu84@studentmail.nait.ca>
2019-09-01 10:41:19 -0600

    Commit Implementation of Service and Repository GetCourses Method

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _dBContext;
+        public CourseRepository(UniversityDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _dBContext.Courses;
+        }
+    }
+}
